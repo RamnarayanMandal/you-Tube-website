e19796218b8d39f9bb66422ef3c30e3093129059
RamnarayanMandal <ramnarayan847231@gmail.com>
2024-04-13 23:05:14 +0530

    setup upload file on coldinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import {v2 as cloudinary} from 'cloudinary'
+import fs from 'fs'
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_KEY_SECRET 
+});
+
+const uploadOnCloudiner = async(localFilePath)=>{
+    try{
+        if(!ocalFilePath) return null;
+        // upload the file on clodinary 
+         const response= await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        // file has been successfully uploaded
+        console.log("file is successfully uploaded", response.url);
+        return response;
+        
+    }catch(error){
+         fs.unlink(localFilePath); // remove the locally saved temporary file as the upload operation got failed 
+      return null;
+}
+
+}
+
+export {uploadOnCloudiner}
